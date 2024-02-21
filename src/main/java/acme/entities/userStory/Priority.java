ad632aa08dc7400acf2b9aa487b5077e74a9a104
yato03 <miguelhs3523@gmail.com>
2024-02-21 12:29:49 +0100

    Student #1 - entities

---
+++ src/main/java/acme/entities/userStory/Priority.java
@@ -0,0 +1,6 @@
+
+package acme.entities.userStory;
+
+public enum Priority {
+	MUST, SHOULD, COULD, WONT
+}
--- src/main/java/acme/entities/userStory/UserStory.java
+++ src/main/java/acme/entities/userStory/UserStory.java
@@ -1,11 +1,8 @@
 
-package acme.entities.projects;
+package acme.entities.userStory;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.validator.constraints.Length;
@@ -18,7 +15,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Project extends AbstractEntity {
+public class UserStory extends AbstractEntity {
 	// Serialisation identifier -----------------------------------------------
 
 	private static final long	serialVersionUID	= 1L;
@@ -26,23 +23,21 @@ public class Project extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{3}-[0-9]{4}")
-	@Column(unique = true)
-	private String				code;
-
-	@NotBlank
 	@Length(max = 75)
 	private String				title;
 
 	@NotBlank
 	@Length(max = 100)
-	private String				abstractText;
-
-	@NotNull
-	private Boolean				hasFatalError;
+	private String				description;
 
 	@PositiveOrZero
-	private Double				cost;
+	private Double				estimatedCost;
+
+	private Priority			priority;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				acceptanceCriteria;
 
 	@URL
 	private String				optionalLink;
