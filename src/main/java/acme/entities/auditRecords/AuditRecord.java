9709a32ee004d12f8269051be26b7180093e981c
Fernando José De Celis Hurtado <ferdehur@alum.us.es>
2024-03-06 11:41:23 +0100

    Grupales: Banner || Individuales: Todas y extra Auditor

---
+++ src/main/java/acme/entities/auditRecords/AuditRecord.java
@@ -0,0 +1,61 @@
+
+package acme.entities.auditRecords;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.entities.codeAudits.CodeAudit;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AuditRecord extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Pattern(regexp = "AU-[0-9]{4}-[0-9]{3}")
+	@Column(unique = true)
+	private String				code;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				auditStartTime;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				auditEndTime;
+
+	@NotNull
+	private Mark				mark;
+
+	@URL
+	private String				optionalLink;
+
+	@ManyToOne
+	@Valid
+	@NotNull
+	private CodeAudit			codeAudit;
+
+}
---
+++ src/main/java/acme/entities/auditRecords/Mark.java
@@ -0,0 +1,8 @@
+
+package acme.entities.auditRecords;
+
+public enum Mark {
+
+	AA, A, B, C, F, FF
+
+}
