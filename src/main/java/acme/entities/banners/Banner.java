fff9cedd124817ab88b03e67ab72a72ce7575866
Fernando José De Celis Hurtado <ferdehur@alum.us.es>
2024-03-08 12:34:25 +0100

    [fix] Arreglo en la entidad banner

--- src/main/java/acme/entities/banners/Banner.java
+++ src/main/java/acme/entities/banners/Banner.java
@@ -4,10 +4,8 @@ package acme.entities.banners;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -16,7 +14,6 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
-import acme.entities.projects.Project;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -59,10 +56,5 @@ public class Banner extends AbstractEntity {
 	private String				link;
 
 	// Relationships ----------------------------------------------------------
-
-	@ManyToOne(optional = false)
-	@Valid
-	@NotNull
-	private Project				project;
 
 }
