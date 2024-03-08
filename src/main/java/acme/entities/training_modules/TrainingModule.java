a44dacea97561b0188d5cee8974e2cc495927937
Gonzalo Navas Remmers <gonnavrem@alum.us.es>
2024-03-08 10:12:49 +0100

    Datos de prueba de Notice y correcciones

--- src/main/java/acme/entities/training_modules/TrainingModule.java
+++ src/main/java/acme/entities/training_modules/TrainingModule.java
@@ -5,8 +5,11 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -16,6 +19,8 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.entities.projects.Project;
+import acme.roles.Developer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,9 +36,10 @@ public class TrainingModule extends AbstractEntity {
 
 	@Column(unique = true)
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$")
 	private String						code;
 
+	@NotNull
 	@Past
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date						creationMoment;
@@ -42,6 +48,7 @@ public class TrainingModule extends AbstractEntity {
 	@Length(max = 100)
 	private String						details;
 
+	@NotNull
 	@Past
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date						updateMoment;
@@ -52,6 +59,21 @@ public class TrainingModule extends AbstractEntity {
 	@URL
 	private String						link;
 
+	@Min(1)
+	private int							totalTime;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@ManyToOne(optional = false)
+	@Valid
 	@NotNull
-	private Integer						totalTime;
+	private Developer					developer;
+
+	@ManyToOne(optional = false)
+	@Valid
+	@NotNull
+	private Project						project;
+
 }
