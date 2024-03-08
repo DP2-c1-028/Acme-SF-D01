21c7955d902f6fac98370aa08e6fc5c4b2756708
yato03 <miguelhs3523@gmail.com>
2024-03-08 14:04:30 +0100

    uml

--- src/main/java/acme/entities/objectives/Objective.java
+++ src/main/java/acme/entities/objectives/Objective.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -42,9 +43,9 @@ public class Objective extends AbstractEntity {
 	private String				description;
 
 	@NotNull
+	@Valid
 	private Priority			priority;
 
-	@NotNull
 	private boolean				status;
 
 	@NotNull
