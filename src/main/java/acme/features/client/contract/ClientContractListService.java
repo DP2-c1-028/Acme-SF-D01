e3fecc0bf5fe6c569a3defcbe6984945b93ae940
Daniel benito Hidalgo <danbenhid@alum.us.es>
2024-04-04 18:57:06 +0200

    Listado de contratos disponible
    Se ha creado el repositorio, servicio, controlador y clases necesarias
    relacionadas a la visualización de información en frontend para mostrar
    el listado de contratos

---
+++ src/main/java/acme/features/client/contract/ClientContractListService.java
@@ -0,0 +1,52 @@
+
+package acme.features.client.contract;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.contracts.Contract;
+import acme.roles.Client;
+
+@Service
+public class ClientContractListService extends AbstractService<Client, Contract> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private ClientContractRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+
+		Collection<Contract> contracts;
+		int clientId;
+
+		clientId = super.getRequest().getPrincipal().getActiveRoleId();
+		contracts = this.repository.findContractsByClientId(clientId);
+
+		super.getBuffer().addData(contracts);
+	}
+
+	@Override
+	public void unbind(final Contract contract) {
+
+		assert contract != null;
+		Dataset dataset;
+
+		dataset = super.unbind(contract, "code", "providerName", "customerName", "instantiationMoment", "budget", "goals");
+
+		super.getResponse().addData(dataset);
+	}
+}
---
+++ src/main/java/acme/features/client/contract/ClientContractRepository.java
@@ -0,0 +1,21 @@
+
+package acme.features.client.contract;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.contracts.Contract;
+
+@Repository
+public interface ClientContractRepository extends AbstractRepository {
+
+	@Query("select c from Contract c where c.client.id = :id")
+	Collection<Contract> findContractsByClientId(int id);
+
+	@Query("select c from Contract c where c.id = :id")
+	Contract findContractById(int id);
+
+}
