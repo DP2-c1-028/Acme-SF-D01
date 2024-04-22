b9072e2219652e5b68b9d164d11e81eca92111ec
Daniel benito Hidalgo <danbenhid@alum.us.es>
2024-04-22 21:11:01 +0200

    actualizado el dashboard para acepytar solo entidades publicadas

--- src/main/java/acme/features/client/dashboard/ClientDashboardRepository.java
+++ src/main/java/acme/features/client/dashboard/ClientDashboardRepository.java
@@ -12,16 +12,16 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface ClientDashboardRepository extends AbstractRepository {
 
-	@Query("select count(p) from ProgressLog p where  p.completeness < :higher and p.completeness >= :lower and p.client.id = :id")
+	@Query("select count(p) from ProgressLog p where p.draftMode = false and p.completeness < :higher and p.completeness >= :lower and p.client.id = :id")
 	int logsBetweenCompletenessValuesForClient(int id, float lower, float higher);
 
-	@Query("select count(p) from ProgressLog p where  p.completeness < :higher and p.client.id =:id")
+	@Query("select count(p) from ProgressLog p where p.draftMode = false and  p.completeness < :higher and p.client.id =:id")
 	int logsBelowCompletenessValue(int id, float higher);
 
-	@Query("select count(p) from ProgressLog p where  p.completeness >= :lower and p.client.id = :id")
+	@Query("select count(p) from ProgressLog p where p.draftMode = false and p.completeness >= :lower and p.client.id = :id")
 	int logsAboveCompletenessValue(int id, float lower);
 
-	@Query("select c.budget from Contract c where c.client.id = :id")
+	@Query("select c.budget from Contract c where c.draftMode = false and  c.client.id = :id")
 	Collection<Money> findAllBudgetsFromClient(int id);
 
 	default double currencyTransformerUsd(final Money initial) {
