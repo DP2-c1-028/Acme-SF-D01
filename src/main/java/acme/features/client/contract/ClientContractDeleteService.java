6b4ca1d64856ca6e3c982c349a973e205d61d325
Daniel benito Hidalgo <danbenhid@alum.us.es>
2024-05-21 01:29:54 +0200

    actulizacion de las clases delete para teenr el coverage bien y actualizacion de los csv para introducir bien los limites

--- src/main/java/acme/features/client/contract/ClientContractDeleteService.java
+++ src/main/java/acme/features/client/contract/ClientContractDeleteService.java
@@ -1,16 +1,11 @@
 
 package acme.features.client.contract;
 
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import acme.client.data.models.Dataset;
 import acme.client.services.AbstractService;
-import acme.client.views.SelectChoices;
 import acme.entities.contracts.Contract;
-import acme.entities.projects.Project;
 import acme.roles.Client;
 
 @Service
@@ -79,23 +74,5 @@ public class ClientContractDeleteService extends AbstractService<Client, Contrac
 		this.repository.delete(contract);
 	}
 
-	@Override
-	public void unbind(final Contract contract) {
-		assert contract != null;
-
-		Dataset dataset;
-		String projectName = this.repository.findProjectById(contract.getProject().getId()).getTitle();
-
-		Collection<Project> projects = this.repository.findlAllProjects();
-		SelectChoices options;
-
-		options = SelectChoices.from(projects, "title", this.repository.findProjectById(contract.getProject().getId()));
-
-		dataset = super.unbind(contract, "code", "project", "providerName", "customerName", "instantiationMoment", "budget", "goals");
-
-		dataset.put("project", projectName);
-		dataset.put("projects", options);
-
-		super.getResponse().addData(dataset);
-	}
+	//AL NO HABER POSIBILIDAD DE DEVOLVER EL FORMULARIO DEBIDO A LOS REQUERIMIENTOS, EL UNBID NO ES NECESARIO DESARROLLARLO
 }
--- src/main/webapp/WEB-INF/views/client/contract/form.jsp
+++ src/main/webapp/WEB-INF/views/client/contract/form.jsp
@@ -18,8 +18,6 @@
 			<acme:submit code="client.contract.form.button.update" action="/client/contract/update" />
 			<acme:submit code="client.contract.form.button.delete" action="/client/contract/delete" />
 			<acme:submit code="client.contract.form.button.publish" action="/client/contract/publish" />
-			
-			<acme:button code="client.contract.form.button.progress-logs" action="/client/progress-log/list?contractId=${id}"/>
 		</jstl:when>
 		<jstl:when test="${_command == 'create'}">
 		<acme:input-select code="client.contract.form.label.project" path="project" choices="${projects}" />
