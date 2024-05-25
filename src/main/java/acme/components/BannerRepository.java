e54f461fdb2957b17e5e7e1930dd69bd54ab8ebe
Daniel benito Hidalgo <danbenhid@alum.us.es>
2024-05-25 13:27:25 +0200

    cambios en el banner repository

--- src/main/java/acme/components/BannerRepository.java
+++ src/main/java/acme/components/BannerRepository.java
@@ -3,7 +3,6 @@ package acme.components;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -12,6 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.client.helpers.MomentHelper;
+import acme.client.helpers.RandomHelper;
 import acme.client.repositories.AbstractRepository;
 import acme.entities.banners.Banner;
 
